859248af424180dbb7db6562912f71b736624e48
Manoj Burri <manojkumarburri@gmail.com>
2023-11-18 19:04:42 +0530

    Initial commit of the web site, README and LICENSE

---
+++ Repository/Repository/RepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryApp.swift
+//  Repository
+//
+//  Created by ManojBurri on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
